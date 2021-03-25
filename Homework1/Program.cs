3adec092fa46b0ef67aabe4b56aa47f83152160d
yinchang0626 <yincahng0626@gmail.com>
2021-03-25 10:09:43 +0800

    重構

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Homework1
 {
@@ -17,7 +18,7 @@ namespace Homework1
             {
                 var line = reader.ReadLine();
                 var values = line.Split(",");
-                Vote vote = new Vote(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9]);
+                Vote vote = Vote.CreateFormCSV(values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], values[8], values[9]);
                 voteList.Add(vote);
 
 
@@ -35,44 +36,52 @@ namespace Homework1
                     resultList.Add(false);
                 }
             }
-            resultList.ForEach(delegate (bool value)
-            {
-                if (value == true)
-                {
-                    passNumber += 1;
+            //resultList.ForEach(delegate (bool value)
+            //{
+            //    if (value == true)
+            //    {
+            //        passNumber += 1;
 
-                }
-            });
+            //    }
+            //});
+
+            passNumber = resultList.Count(x => x == true);
             Console.WriteLine("法案通過率" + ((passNumber) / (resultList.Count) * 100).ToString() + "%");
 
         }
     }
-    internal class Vote
-    {
-        public string Number { get; set; }//a
-        public string AllPopulation { get; set; }//b
-        public string Agree { get; set; }//c
-        public string NotAgree { get; set; }//d
-        public string Avaliable { get; set; }//e
-        public string NotAvalible { get; set; }//f
-        public string RealPopulation { get; set; }//g
-        public string VotingRate { get; set; }//h
-        public string AgreeToAllRate { get; set; }//i
-        public string Result { get; set; }//j
-        public Vote() { }
-        public Vote(string a, string b, string c, string d, string e, string f, string g, string h, string i, string j)
-        {
-            this.Number = a;
-            this.AllPopulation = b;
-            this.Agree = c;
-            this.NotAgree = d;
-            this.Avaliable = e;
-            this.NotAvalible = f;
-            this.RealPopulation = g;
-            this.VotingRate = h;
-            this.AgreeToAllRate = i;
-            this.Result = j;
-        }
 
+}
+internal class Vote
+{
+    public string Number { get; set; }//a
+    public string AllPopulation { get; set; }//b
+    public string Agree { get; set; }//c
+    public string NotAgree { get; set; }//d
+    public string Avaliable { get; set; }//e
+    public string NotAvalible { get; set; }//f
+    public string RealPopulation { get; set; }//g
+    public string VotingRate { get; set; }//h
+    public string AgreeToAllRate { get; set; }//i
+    public string Result { get; set; }//j
+    public Vote() { }
+    private Vote(string a, string b, string c, string d, string e, string f, string g, string h, string i, string j)
+    {
+        this.Number = a;
+        this.AllPopulation = b;
+        this.Agree = c;
+        this.NotAgree = d;
+        this.Avaliable = e;
+        this.NotAvalible = f;
+        this.RealPopulation = g;
+        this.VotingRate = h;
+        this.AgreeToAllRate = i;
+        this.Result = j;
     }
+
+    public static Vote CreateFormCSV(string a, string b, string c, string d, string e, string f, string g, string h, string i, string j)
+    {
+        return new Vote(a, b, c, d, e, f, g, h, i, j);
+    }
+
 }
